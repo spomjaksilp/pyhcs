95b0e3866036c0e29fe9c0656b14b061ba4de8f5
Suthep Pomjaksilp <pomjaksi@physik.uni-kl.de>
2020-07-28 14:13:39 +0200

    Adds device limits

--- src/hcs/device.py
+++ src/hcs/device.py
@@ -24,14 +24,22 @@ class HCS:
         "I": None
     }
 
-    def __init__(self, port="/dev/ttyUSB0", max_voltage=None, max_current=None):
+    DEVICE_LIMITS = {
+        "U": None,
+        "I": None,
+    }
+
+    def __init__(self, port="/dev/ttyUSB0", limit_voltage=None, limit_current=None):
         logging.info("Connecting to serial device at {}".format(port))
         self.port = port
-        logging.info("Configuration soft-limits to {}V and {}A".format(max_voltage, max_current))
-        self.max_voltage = max_voltage
-        self.max_current = max_current
+        logging.info("Configuration soft-limits to {}V and {}A".format(limit_voltage, limit_current))
+        self.limit_voltage = limit_voltage
+        self.limit_current = limit_current
         self.serial = serial.Serial(port=self.port, baudrate=9600, timeout=0.5)
-        self.__enter__()
+        self.max_voltage, self.max_current = self.get_max()
+        logging.info("Max ratings for {}: {}V and {}A".format(self.port, self.max_voltage, self.max_current))
+        assert self.limit_voltage is None or self.limit_voltage <= self.max_voltage, "Voltage limit > device maximum"
+        assert self.limit_current is None or self.limit_current <= self.max_voltage, "Current limit > device maximum"
 
     def __enter__(self):
         return self
@@ -39,6 +47,28 @@ class HCS:
     def __exit__(self, exc_type, exc_val, exc_tb):
         logging.info("Terminating serial connection to {}".format(self.port))
         self.serial.__exit__()
+
+    @property
+    def min_voltage(self):
+        """
+        Returns the minimal voltage for this device
+        :return: (float) minimal voltage
+        """
+        if self.DEVICE_LIMITS["U"] is None:
+            raise NotImplementedError
+        else:
+            return self.DEVICE_LIMITS["U"][0]
+
+    @property
+    def min_current(self):
+        """
+        Returns the minimal current for this device
+        :return: (float) minimal voltage
+        """
+        if self.DEVICE_LIMITS["I"] is None:
+            raise NotImplementedError
+        else:
+            return self.DEVICE_LIMITS["I"]
 
     def _write(self, cmd):
         """
@@ -73,6 +103,14 @@ class HCS:
             return answer
         else:
             raise Exception("Command {} did not receive ACK".format(cmd.decode()))
+
+    def get_max(self):
+        """
+        Queries max voltage and current
+        :return: (tuple) max_voltage, max_voltage
+        """
+        result = self._execute(b"GMAX")
+        return self._parse_result(result, self.SET_DECIMALS)
 
     @staticmethod
     def _parse_result(result, decimals):
@@ -133,8 +171,12 @@ class HCS:
         :return:
         """
         assert self.max_voltage is None or voltage <= self.max_voltage,\
-            "Invalid range.\n{} is larger than soft limit of {}".format(voltage, self.max_voltage)
-        assert voltage >= 0.8, "Negative or voltage <= 0.8 given"
+            "Invalid range! {}V is larger than soft limit of {}V".format(voltage, self.max_voltage)
+        assert voltage > 0, "Negative voltage given"
+        if voltage < self.min_voltage:
+            logging.warning("Given voltage {}V < {}V minimum, setting to minimum voltage".format(voltage,
+                                                                                              self.min_voltage))
+            voltage = self.min_voltage
         voltage_bytes = "{:0{}d}".format(round(voltage * 10**self.SET_DECIMALS["U"]),
                                          self.SET_DECIMALS["U"] + 2).encode()
         self._execute(b"VOLT" + voltage_bytes)
@@ -147,8 +189,12 @@ class HCS:
         :return:
         """
         assert self.max_current is None or current <= self.max_current,\
-            "Invalid range.\n{} is larger than soft limit of {}".format(current, self.max_current)
-        assert current >= 0.1, "Negative or zero current given"
+            "Invalid range! {}A is larger than soft limit of {}A".format(current, self.max_current)
+        assert current > 0, "Negative current given"
+        if current < self.min_current:
+            logging.warning("Given current {}A < {}A minimum, setting to minimum current".format(current,
+                                                                                                 self.min_current))
+            current = self.min_current
         current_bytes = "{:0{}d}".format(round(current * 10 ** self.SET_DECIMALS["I"]),
                                          self.SET_DECIMALS["I"] + 2).encode()
         self._execute(b"CURR" + current_bytes)
@@ -164,4 +210,9 @@ class HCS3202(HCS):
     DISPLAY_DECIMALS_I = {
         "U": 2,
         "I": 2
+    }
+
+    DEVICE_LIMITS = {
+        "U": 0.8,
+        "I": 0.1,
     }
